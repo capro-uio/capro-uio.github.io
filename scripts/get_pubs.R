2181ae7336b01ff45c31d7d5051feb7777ace536
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2025-03-17 19:51:07 +0100

    style code

--- scripts/get_pubs.R
+++ scripts/get_pubs.R
@@ -1,48 +1,50 @@
-library(scholar)
 library(jsonlite)
+library(scholar)
 
-get_pubs <- function(id, file){
-  if(is.null(id)) return()
+get_pubs <- function(id, file) {
+  if (is.null(id)) return()
   dt <- get_publications(id)
-  dt <- dt[order(dt$year, decreasing = TRUE),]
+  dt <- dt[order(dt$year, decreasing = TRUE), ]
   # dt$url <- sprintf("https://scholar.google.no/citations?view_op=view_citation&citation_for_view=%s", dt$pubid)
-  dt$author <- sapply(dt$author, function(x){
+  dt$author <- sapply(dt$author, function(x) {
     k <- strsplit(x, ", ")[[1]]
-    if(length(k) > 6){
-      k <- paste0(k[1:3], collapse=", ")
-      k <- paste(k, "et al.", collapse=" ")
-    }else{
+    if (length(k) > 6) {
+      k <- paste0(k[1:3], collapse = ", ")
+      k <- paste(k, "et al.", collapse = " ")
+    } else {
       n <- length(k)
-      j <- paste0(k[1:(n-1)], collapse=", ")
-      k <- paste(c(j, "&", k[n]), collapse=" ")
+      j <- paste0(k[1:(n - 1)], collapse = ", ")
+      k <- paste(c(j, "&", k[n]), collapse = " ")
     }
     k
   })
-  dt$journal <- ifelse(dt$journal == "", "", sprintf("%s %s", dt$journal, dt$number))
-  dt <- dt[, c("year","author", "title", "journal", "pubid")]
+  dt$journal <- ifelse(
+    dt$journal == "",
+    "",
+    sprintf("%s %s", dt$journal, dt$number)
+  )
+  dt <- dt[, c("year", "author", "title", "journal", "pubid")]
   row.names(dt) <- NULL
 
   dir.create(dirname(file), showWarnings = FALSE, recursive = TRUE)
   cat("writing to: ", normalizePath(file), "\n")
-  jsonlite::write_json(dt,
-                       file,
-                       pretty = TRUE,
-                       auto_unbox = TRUE)
+  jsonlite::write_json(dt, file, pretty = TRUE, auto_unbox = TRUE)
 }
 
 
-team <- list.files("content/team",
-           recursive = TRUE,
-           pattern = "^index.en.md$",
-           full.names = TRUE)
+team <- list.files(
+  "content/team",
+  recursive = TRUE,
+  pattern = "^index.en.md$",
+  full.names = TRUE
+)
 
 ids <- team |>
   lapply(readLines) |>
-  sapply(function(x){
+  sapply(function(x) {
     j <- x[grep("gscholar", x)] |>
       strsplit('\"')
-    if(length(j) > 0)
-      j[[1]][2]
+    if (length(j) > 0) j[[1]][2]
   })
 
 j <- mapply(
@@ -50,6 +52,3 @@ j <- mapply(
   id = ids,
   file = paste0("data/team/pubs/", basename(dirname(team)), ".json")
 )
-
-
-
